a34d604f341b49367a273442919881c094a46839
henilhpatel <patel.henilh@northeastern.edu>
2026-01-30 18:33:23 -0500

    changed the dataset and used Logistic Regression model

--- Labs/Docker_Labs/Lab1/src/main.py
+++ Labs/Docker_Labs/Lab1/src/main.py
@@ -1,22 +1,22 @@
 # Import necessary libraries
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 import joblib
 
 if __name__ == '__main__':
-    # Load the Iris dataset
-    iris = load_iris()
-    X, y = iris.data, iris.target
+    # Load the wine dataset
+    wine_dataset = load_wine()
+    X, y = wine_dataset.data, wine_dataset.target
 
     # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # Train a Random Forest classifier
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model = LogisticRegression(max_iter=1000, solver='liblinear', random_state=42)
     model.fit(X_train, y_train)
 
     # Save the model to a file
-    joblib.dump(model, 'iris_model.pkl')
+    joblib.dump(model, 'wine_classification_model.pkl')
     
     print("The model training was successful")
