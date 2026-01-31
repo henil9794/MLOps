5ae4d637d359af318211006aeb5f2cf7d92f3aaf
henil9794 <henilpatel9794@gmail.com>
2026-01-30 19:30:37 -0500

    updated main.py file

--- Labs/Docker_Labs/Lab1/src/main.py
+++ Labs/Docker_Labs/Lab1/src/main.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Train a Random Forest classifier
+    # Train a Logistic Regression Model
     model = LogisticRegression(max_iter=1000, solver='liblinear', random_state=42)
     model.fit(X_train, y_train)
 
