f9eb3cefc5da89bd6c96eef6620d03b08dc6acaf
henil9794 <henilpatel9794@gmail.com>
2026-02-13 18:40:32 -0500

    updated predicted.py and added predict_confidence function

--- Labs/API_Labs/FastAPI_Labs/src/predict.py
+++ Labs/API_Labs/FastAPI_Labs/src/predict.py
@@ -1,13 +1,13 @@
 import joblib
 
+model = joblib.load("../model/wine_model.pkl")
 def predict_data(X):
-    """
-    Predict the class labels for the input data.
-    Args:
-        X (numpy.ndarray): Input data for which predictions are to be made.
-    Returns:
-        y_pred (numpy.ndarray): Predicted class labels.
-    """
-    model = joblib.load("../model/iris_model.pkl")
     y_pred = model.predict(X)
     return y_pred
+
+def predict_confidence(X):
+    if model is None:
+        raise Exception("Model is not loaded")
+    
+    probabilities = model.predict_proba(X)
+    return probabilities[0]
